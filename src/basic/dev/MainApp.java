d54fb17bb3b778047f186ee59608eb6f731008fe
nhattan22399 <linhatan99@gmail.com>
2021-04-22 16:43:39 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,27 @@
+package basic.dev;
+
+public class MainApp {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Tao thong tin xe car
+		String nameCar;
+		String nameManufacturer;
+		float price;
+		String color;
+		//Tao thong tin worker
+		String nameWorker;
+		String address;
+		String phone;
+		float coefficient;
+		//Tao thong tin Student
+		String name;
+		int id;
+		String cmnd;
+		String nameClass;
+		String nameSchool;
+		float dtb;
+	}
+
+}
